7b439c4788fb0fbbed4d84cb318a4659388712bb
Rodolfo Campos <camposer@gmail.com>
2012-08-16 00:47:14 +0200

    i18n added and documentation

--- sqlite/public/javascripts/views/index.js
+++ sqlite/public/javascripts/views/index.js
@@ -1,3 +1,19 @@
+/** 
+* @fileOverview Index code-behind (express-examples/sqlite)
+*
+* @author Rodolfo Campos <camposer at gmail dot com>
+* @version 1.0
+*/
+
+/**
+ * Constants for use in userForm method (Add or Modify)
+ */
+var BUTTON_ADD_USER = 'Add user',
+    BUTTON_MODIFY_USER = 'Modify user';
+
+/**
+ * Method to be executed when the document is ready (main method)
+ */
 $(document).ready(function() {
 	// Retrieving users
 	getUsers();
@@ -5,15 +21,35 @@ $(document).ready(function() {
 	// Initializing addUser button
 	$('#addUser').button();
 	$('#addUser').click(function() { 
-		$("#userForm").dialog(userForm('Add')); // Have to specify which operation before openning the dialog
+		$("#userForm").dialog(userForm(BUTTON_ADD_USER)); // Have to specify which operation before openning the dialog
 		$('#userForm').dialog('open'); 
 	});
 
 	// Initializing dialogs
 	$("#userForm").dialog(userForm());
 	$("#deleteUserConfirm").dialog(deleteUserConfirm());
+
+	// Initializing texts (i18n)
+	i18n();
 });
 
+/**
+ * Headers to be used by jQuery Template for compiling usersTable header
+ * @field
+ */
+var headers = {
+	id: $.i18n._('Id'),
+	name: $.i18n._('Name'),
+	operation: $.i18n._('Operation')
+};
+
+/**
+ * Retrieves users from REST API and fills usersTable. While usersTable is
+ * compiled, links and behaviours for operations (modify and delete) are added.
+ * - The data retrievement is made using jQuery, through an AJAX GET HTTP Request
+ * - usersTable is filled using jQuery Template
+ * @function
+ */
 var getUsers = function() {
 	$.ajax({
 		type : 'GET',
@@ -22,24 +58,26 @@ var getUsers = function() {
 		success : function(data, textStatus, jqXHR) {
 			if (data.length > 0) {
 				$("#usersContainer").html('');
-				$("#usersTableTemplate").tmpl(null).appendTo("#usersContainer");				
+				$("#usersTableTemplate").tmpl(headers).appendTo("#usersContainer");				
 				$("#usersTrTemplate").tmpl(data).appendTo("#usersTable > tbody");
 				
 				// Adding operations
 				$.each($("#usersTable > tbody > tr"), function(index, value) {
-					// Modifying id anchor
+					// Modifying id anchor (modify operation)
 					var name = $(':nth-child(2)', this).html();
 					$(':nth-child(1) > a', this).attr('href', '#');
 					$(':nth-child(1) > a', this).click(function () {
 						var id = $(this).html();
 						
-						$("#userForm").dialog(userForm('Modify', { id: id, name: name }));
+						$("#userForm").dialog(userForm(BUTTON_MODIFY_USER, { id: id, name: name }));
 						$('#userForm').dialog('open'); 						
 					});
+
 					
-					// Modifying delete anchor
+					// Modifying delete anchor (delete operation)
+					$(':nth-child(3) > a', this).html($.i18n._('Delete').toLowerCase()); 
 					$(':nth-child(3) > a', this).attr('href', '#'); 
-					$(':nth-child(3) > a', this).attr('name', $(':nth-child(1) > a', this).html()); // User id stores into name anchor's property  
+					$(':nth-child(3) > a', this).attr('name', $(':nth-child(1) > a', this).html()); 
 					$(':nth-child(3) > a', this).click(function () {
 						$("#deleteUserConfirm").dialog(deleteUserConfirm(
 								$(this).attr('name')
@@ -59,6 +97,13 @@ var getUsers = function() {
 	});
 }
 
+/**
+ * Adds user using the specified name
+ * - The user is added using jQuery, through an AJAX PUT HTTP Request
+ * - If the AJAX call succeed getUsers method is invoked
+ * @param {String} name User's name
+ * @function
+ */
 var addUser = function(name) {
 	$.ajax({
 		type : 'PUT',
@@ -80,6 +125,14 @@ var addUser = function(name) {
 	});
 }
 
+/**
+ * Updates user for the specified id
+ * - The user is updated using jQuery, through an AJAX POST HTTP Request
+ * - If the AJAX call succeed getUsers method is invoked
+ * @param {Integer} id User's id
+ * @param {String} name User's name
+ * @function
+ */
 var updateUser = function(id, name) {
 	$.ajax({
 		type : 'POST',
@@ -102,6 +155,13 @@ var updateUser = function(id, name) {
 	});
 }
 
+/**
+ * Removes a user with the specified id
+ * - The user is removed using jQuery, through an AJAX DELETE HTTP Request
+ * - If the AJAX call succeed getUsers method is invoked
+ * @param {Integer} id User's id
+ * @function
+ */
 var deleteUser = function(id) {
 	$.ajax({
 		type : 'DELETE',
@@ -123,9 +183,44 @@ var deleteUser = function(id) {
 	});
 }
 
-var userForm = function(operation, user) { // This id can be managed with a hidden field, but I think this way is "cleanner"
+/**
+ * Defines userForm dialog for adding or modifying users
+ * @param {String} operation BUTTON_ADD_USER or BUTTON_MODIFY_USER
+ * @param {Object} user User
+ * @function
+ */
+var userForm = function(operation, user) { 
+	// Defining buttons using eval for i18n support
+	eval("var buttons = " + 
+		"{" + 
+			"'" + $.i18n._('Accept') + "' : function() {" +
+			"	var name = $('#userFormName').val();" +
+
+			"	if (name && name.trim() != '') {" + 
+			"		if (operation == BUTTON_ADD_USER)" +
+			"			addUser(name);" +
+			"		else" + 
+			"			updateUser(user.id, name);" +
+						
+			"		$(this).dialog('close');" + 
+			"	} else {" + 
+			"		$('#userFormError').addClass('ui-state-error');" +
+			"		$('#userFormError').html(" +
+			"				'You have to specify a valid name'" + 
+			"		);" +
+			"	}" +
+
+			"	$('#userFormName').val('');" +
+			"}, " +
+			"'" + $.i18n._('Cancel') + "' : function() {" +
+			"	$('#userFormName').val('');" +
+			"	$(this).dialog('close');" +
+			"}" + 
+		"}"
+	);
+
 	return {
-		title : operation + ' user',
+		title :  $.i18n._(operation),
 		autoOpen : false,
 		height : 250,
 		width : 350,
@@ -134,48 +229,46 @@ var userForm = function(operation, user) { // This id can be managed with a hidd
 			if (user && user.name) 
 				$('#userFormName').val(user.name); // Initializing with user.name if its provided
 		},
-		buttons : {
-			'Accept' : function() {
-				var name = $('#userFormName').val();
-				
-				if (name && name.trim() != '') {
-					if (operation=='Add') 
-						addUser(name);						
-					else
-						updateUser(user.id, name);
-						
-					$(this).dialog('close');
-				} else {
-					$('#userFormError').addClass('ui-state-error');
-					$('#userFormError').html(
-							'You have to specify a valid name');
-				}
-
-				$('#userFormName').val('');
-			},
-			'Cancel' : function() {
-				$('#userFormName').val('');
-				$(this).dialog('close');
-			}
-		}
+		buttons : buttons
 	};
-};
+}
 
+/**
+ * Defines deleteUserConfirm dialog for deleting users
+ * @param {Integer} id User's id
+ * @function
+ */
 var deleteUserConfirm = function(id) {
+	// Defining buttons using eval for i18n support
+	eval("var buttons = { " +
+			"'" + $.i18n._('Delete') + "' : function() {" +
+			"	deleteUser(id);" +
+			"	$(this).dialog('close');" +
+			"}," +
+			"'" + $.i18n._('Cancel') + "' : function() {" +
+			"	$(this).dialog('close');" +
+			"}" +
+		"}"
+	);
+
 	return {
+		title : $.i18n._('Delete user'),
 		autoOpen : false,
 		resizable : false,
 		height : 220,
 		width : 400,
 		modal : true,
-		buttons : {
-			"Delete" : function() {
-				deleteUser(id);
-				$(this).dialog("close");
-			},
-			Cancel : function() {
-				$(this).dialog("close");
-			}
-		}
+		buttons : buttons
 	};
+}
+
+/**
+ * Set HTML texts considering i18n
+ * @param {Integer} id User's id
+ * @function
+ */
+var i18n = function() {
+	$('#addUser').val($.i18n._(BUTTON_ADD_USER));
+	$('#userFormName').html($.i18n._('Name') + ':');
+	$('#deleteUserConfirmMsg').html($.i18n._('The user will be permanently deleted and cannot be recovered. Are you sure?'));
 }
